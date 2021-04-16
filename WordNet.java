10da30254b68191eb9951a41d55f7d14f7e8decc
Vastera <vastera@163.com>
2021-04-16 09:58:01 +0800

    use the first method in  https://app.yinxiang.com/shard/s39/nl/24605222/c02f2241-9135-4c79-b92c-d9c49f497e96
    transverse all the reachable vertexes of first node, to check if they connect second node. This method maybe not the optimal one, and doesn't involve with breadth first search.

--- WordNet.java
+++ WordNet.java
@@ -1,14 +1,19 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.Stack;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class WordNet {
     private RedBlackBST<String, Integer> wordsBST = new RedBlackBST<String, Integer>();
     private String[] words; // store the nodes of synsets
     private Digraph wordMap; // store the graph of wordnet
+    private int V;
+    HashMap<Integer, Integer> topoHash;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -24,6 +29,7 @@ public class WordNet {
         }
         words = words0.toArray(new String[words0.size()]);
         wordMap = new Digraph(words.length);
+        V = wordMap.V();
         In inAdjcences = new In(hypernyms);
         while (inAdjcences.hasNextLine()) {
             String[] adj = inAdjcences.readLine().split(","); // read the hypernyms
@@ -35,7 +41,6 @@ public class WordNet {
     }
 
     private class depthFirstSearch { // to check whether the digraph is rooted DAG
-        private int V;
         private int[] marked;
         // 0 is unvisited, 1 is visited in the current path, -1 is visited before the current path
         private Digraph digraph;
@@ -43,7 +48,6 @@ public class WordNet {
         private int root;
 
         public depthFirstSearch(Digraph digraph) {
-            V = words.length;
             marked = new int[V];
             this.digraph = digraph;
             reDigraph = digraph.reverse();
@@ -90,7 +94,6 @@ public class WordNet {
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
-        int V = words.length;
         if (v < 0 || v >= V)
             throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
     }
@@ -115,65 +118,180 @@ public class WordNet {
     public String sap(String nounA, String nounB) {
         if (nounA == null || nounB == null)
             throw new IllegalArgumentException("the input argument is null!");
-        int V = wordMap.V();
         // get the index of nounA and nounB
         int numA = wordsBST.get(nounA);
         int numB = wordsBST.get(nounB);
-        // deep copy of digraph into unDiagraph
-        Digraph unDigraph = new Digraph(V);
-        for (int i = 0; i < V; i++) {
-            for (int j : wordMap.adj(i)) {
-                unDigraph.addEdge(i, j);
-                unDigraph.addEdge(j, i);
-            }
-        }
+        // int[] edgeTo = new int[V]; // store the path for retrieve
+        // edgeTo[numA] = numA; // the first terminal node
+
         // BFS to find the shortest ancestral path
-        Queue<Integer> path = new Queue<Integer>();
-        boolean[] marked = new boolean[V];
-        int[] edgeTo = new int[V]; // store the path for retrieve
-        edgeTo[numA] = numA;
-        path.enqueue(numA);
-        int v;
-        while (!path.isEmpty()) {
-            v = path.dequeue();
-            if (v == numB) {
+        // Queue<Integer> path = new Queue<Integer>(); // store the ancestral path
+        // store the words by a hash table in the topological order
+        topoHash = new HashMap<Integer, Integer>();
+        Integer[] topoArray = topologicalOrder();
+        int i = 0;
+        for (int word : topologicalOrder()) {
+            topoHash.put(word, i);
+            i++;
+        }
+        // set X as the smaller one of numA and numB, while set Y as the bigger one
+        int X = Math.min(numA, numB);
+        int Y = Math.max(numA, numB);
+        // find all the reachable nodes of X by using depth first search staring from X
+        // initialize a minheap to store the reachable nodes of X
+        int[] xConntedComponent = new reachability(X).XReachVertexes();
+        // store the reachable nodes of X as an array and a hashtable (XPathHash) for searching, respectively
+        HashMap<Integer, Integer> XPathHash = new HashMap<Integer, Integer>(
+                xConntedComponent.length);
+        for (int j = 0; j < xConntedComponent.length; j++) {
+            XPathHash.put(xConntedComponent[j], j);
+        }
+        // initialize a stack to store the path from Y to the common ancestral
+        // Stack<Integer> YUpward = new Stack<Integer>();
+        // transverse the XPathHash to find out whether there is Y, and Y is the common ancestor
+        do {
+            // if Y exists in the XPathHash, then combine the path to common ancestor from X and the original Y
+            if (XPathHash.containsKey(Y)) {
+                // find the shortest path from X to current Y using Breadth first search
+                // shortestPath sp = new shortestPath(X, Y);
                 break;
             }
-            marked[v] = true;
-            for (int i : unDigraph.adj(v)) {
-                if (!marked[i]) {
-                    marked[i] = true;
-                    path.enqueue(i);
-                    edgeTo[i] = v;
+            else {
+                // if Y doesn't exist in the XPathHash, then make the nearest adjacences of Y in the topological order and repeat searching
+                int yIndex = topoArray.length - 1;
+                for (int j : wordMap.adj(Y)) {
+                    if (topoHash.get(j) < yIndex)
+                        yIndex = topoHash.get(j);
                 }
+                Y = topoArray[yIndex];
             }
-        }
-        int ancestralNum = numB;
-        while (edgeTo[ancestralNum] != ancestralNum) { // before the terminal of the path
-            int next = edgeTo[ancestralNum];
-            // if there is path from current vertex to the next vertex,
-            // then continue, or current is ancestral number
-            boolean contains = false;
-            for (int j : wordMap.adj(ancestralNum))
-                if (j == next) {
-                    contains = true;
-                    break;
-                }
-            if (contains)
-                ancestralNum = next;
-            else
-                break;
-        }
-        return words[ancestralNum];
+        } while (topoHash.get(Y) < topoArray.length);
+        return Integer.toString(Y);
     }
 
+    // find the shortest path between two vertex
+    // private class shortestPath {
+    //     Stack<Integer> SP; // record the shortest path
+    //     boolean[] marker; // visiting status marker
+    //     Queue<Integer> q;
+    //     int[] edgeTo; // record the shorest path
+    //     int start;
+    //     int end;
+    //
+    //     public shortestPath(int start, int end) {
+    //         this.start = start;
+    //         this.end = end;
+    //         SP = new Stack<Integer>();
+    //         marker = new boolean[V];
+    //         q = new Queue<Integer>();
+    //         edgeTo = new int[V];
+    //
+    //         q.enqueue(start);
+    //         marker[start] = true;
+    //         while (!q.isEmpty()) {
+    //             int v = q.dequeue();
+    //             for (int w : wordMap.adj(v)) {
+    //                 if (!marker[w]) {
+    //                     marker[v] = true;
+    //                     q.enqueue(w);
+    //                     edgeTo[w] = v;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     public Iterable<Integer> path() {
+    //         while (start != end) {
+    //             SP.push(end);
+    //             end = edgeTo[end];
+    //         }
+    //         return SP;
+    //     }
+    // }
+
+    private class reachability {
+        private boolean[] marked; // marker of visited status
+        private PriorityQueue<Integer> XMinHeap;
+        private int[] XAdj;
+
+        public reachability(int X) {
+            // X is the source vertex
+            // define a min heap for the reachable vertexes of X according to the topological order
+            XMinHeap = new PriorityQueue<Integer>(V, new Comparator<Integer>() {
+                public int compare(Integer a, Integer b) {
+                    return topoHash.get(b) - topoHash.get(a);
+                }
+            });
+            XAdj = new int[V];
+            marked = new boolean[V];
+            dfs(X);
+        }
+
+        private void dfs(int v) {
+            marked[v] = true;
+            for (int i : wordMap.adj(v)) {
+                if (!marked[i]) {
+                    XMinHeap.add(i);
+                    dfs(i);
+                }
+            }
+        }
+
+        public int[] XReachVertexes() {
+            int i = 0;
+            while (!XMinHeap.isEmpty()) {
+                XAdj[i] = XMinHeap.poll();
+                i++;
+            }
+            return XAdj;
+        }
+    }
+
+    // find the topological order of the whole wordnet
+    private class topological {
+        boolean[] marked; // markers for depth first search
+        Stack<Integer> topoStack;
+
+        public topological() {
+            marked = new boolean[V];
+            topoStack = new Stack<Integer>();
+            for (int i = 0; i < V; i++)
+                if (!marked[i]) topoDfs(i);
+        }
+
+        private void topoDfs(int v) {
+            marked[v] = true;
+            for (int i : wordMap.adj(v))
+                if (!marked[i]) topoDfs(i);
+            topoStack.push(v);
+        }
+
+        public Iterable<Integer> order() {
+            return topoStack;
+        }
+
+    }
+
+    public Integer[] topologicalOrder() {
+        topological topo = new topological();
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        for (int i : topo.order()) {
+            ret.add(i);
+        }
+        // int[] ans = new int[ret.size()];
+        int size = ret.size();
+        return ret.toArray(new Integer[size]);
+    }
     // do unit testing of this class
 
     public static void main(String[] args) {
         WordNet wordnet = new WordNet(args[0], args[1]);
-        System.out.println(wordnet.words[1]);
+        // System.out.println(wordnet.words[1]);
         String[] wds = wordnet.words;
-        System.out.println(wordnet.sap(wds[1], wds[4]));
+        // for (int i : wordnet.topologicalOrder()) {
+        //     System.out.println(i);
+        // }
+        System.out.println(wordnet.sap(wds[0], wds[2]));
 
     }
 }
